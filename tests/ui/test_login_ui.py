a4943995a7d2959c1f674999cd07829bc237579e
Itzik <itzikv3@gmail.com>
2023-02-21 13:55:22 +0200

    before merge

--- tests/ui/test_login_ui.py
+++ tests/ui/test_login_ui.py
@@ -1,3 +1,5 @@
+import logging
+
 import pytest
 from selenium.webdriver.common.by import By
 
@@ -12,35 +14,47 @@ USERNAME = "itzikv3@gmail.com"
 PASSWORD = "itzikpass"
 
 
+# logger = get_logger()
+
+# Set up the logger
+# logging.getLogger(__name__)
+# logger.setLevel(logging.INFO)
+# formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+# handler = logging.StreamHandler()
+# handler.setFormatter(formatter)
+# logger.addHandler(handler)
+
+
 # @pytest.fixture it's like before test
 @pytest.fixture
-def setup():
+def driver():
     driver = setup_driver_options()
-    login_page = LoginPage(driver)
-    yield login_page, driver
+    yield driver
     driver.close()
 
 
-def test_valid_login_ui(setup):
-    login_page = setup[0]
-    driver = setup[1]
-
+def test_valid_login_ui(driver):
+    login_page = LoginPage(driver)
     login_page.login(USERNAME, PASSWORD)
 
+    your_work_css_locator = "css-kwc091"
     your_work_element = WebDriverWait(driver, 20).until(
-        EC.visibility_of_element_located((By.CLASS_NAME, "css-kwc091")))
+        EC.visibility_of_element_located((By.CLASS_NAME, your_work_css_locator)))
     assert your_work_element.text == "Your work"
 
 
-def test_invalid_username_login_ui(setup):
-    login_page = setup[0]
-    driver = setup[1]
+def test_invalid_username_login_ui(driver):
+    login_page = LoginPage(driver)
     login_page.set_username("INVALID_USERNAME")
 
+    continue_xpath_locator = "//span[text()='Continue']"
     continue_element = WebDriverWait(driver, 20).until(
-        EC.visibility_of_element_located((By.XPATH, "//span[text()='Continue']")))
+        EC.visibility_of_element_located((By.XPATH, continue_xpath_locator)))
 
     assert continue_element.text == "Continue"
-
+    # logger.info("logged in successfully")
+    # logger.info("logged in successfully")
+    # logger.info("logged in successfully")
+    # logger.info("logged in successfully")
 
 # def test_invalid_password_login_ui():
