0691d67ead620c33b76473e6870fdee59412f748
Itzik <itzikv3@gmail.com>
2023-02-21 14:41:01 +0200

    logger+structure

--- src/jira_test_framework/ui/login_page.py
+++ src/jira_test_framework/ui/login_page.py
@@ -4,8 +4,12 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
+from src.jira_test_framework.utils import get_logger
+
 
 class LoginPage():
+    logger = get_logger()
+
     base_url = "https://automat-ct.atlassian.net/jira/your-work"
     username_name_locator = "username"
     password_name_locator = "password"
@@ -13,10 +17,11 @@ class LoginPage():
     def __init__(self, driver):
         self.driver = driver
         self.driver.get(self.base_url)
+        self.logger.info("open url: " + self.base_url)
 
     def login(self, username, password):
+        self.logger.info("start login process")
         self.set_details(username, password)
-        # return self.driver
 
     def set_details(self, username, password):
         self.set_username(username)
@@ -27,6 +32,7 @@ class LoginPage():
             EC.visibility_of_element_located((By.NAME, self.username_name_locator)))
         username_element.send_keys(username)
         username_element.send_keys(Keys.RETURN)
+        self.logger.info("username: " + username + " was set")
 
     # this method can execute only after set_username()
     def set_password(self, password):
@@ -34,3 +40,4 @@ class LoginPage():
             EC.visibility_of_element_located((By.NAME, self.password_name_locator)))
         password_element.send_keys(password)
         password_element.send_keys(Keys.RETURN)
+        self.logger.info("password: " + password + " was set")
