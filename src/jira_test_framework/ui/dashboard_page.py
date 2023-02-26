a7cae518873420743582242decdc9bcc48486460
Itzik <itzikv3@gmail.com>
2023-02-26 20:51:44 +0200

    log + assertions

--- src/jira_test_framework/ui/dashboard_page.py
+++ src/jira_test_framework/ui/dashboard_page.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from src.logger import logger
+
 
 class DashboardPage():
     project_elements_css_locator = 'p[data-testid="deep-dive-card-content--project-name-heading"]'
@@ -20,8 +22,30 @@ class DashboardPage():
     def __init__(self, driver):
         self.driver = driver
 
-    # TODO - use the project_name
+    def search(self, issue_value):
+        # TODO - ask, i thought to write here log, but it feels to much, what do you say?
+        time.sleep(2)
+        search_element = self.driver.find_element(By.XPATH, self.search_element_locator)
+        search_element.click()
+        search_element.send_keys(issue_value)
+        search_element.click()
+
+        time.sleep(1)
+        return self.driver.find_element(By.XPATH,
+                                        f"//*[@id='ak-main-content']//div[@data-test-id='software-backlog.card-list.id-2']//div[contains(., '{issue_value}')]")
+
+    def is_issue_exist(self, issue_value):
+        # this is another options to check if the element finds.. - if int(is_issue_exist.size['width']) > 0
+        try:
+            self.search(issue_value)
+            clear_element = self.driver.find_element(By.XPATH, '//span[@aria-label="Clear"]')
+            clear_element.click()
+            return True
+        except:
+            return False
+
     def go_to_project(self, project_name):
+        logger.info("open project: " + project_name)
         project_elements_xpath_locator = f'// p[contains(text(), "{project_name}")]'
 
         project_elements = WebDriverWait(self.driver, 30).until(
@@ -31,11 +55,15 @@ class DashboardPage():
         project_elements[0].click()
 
     def click_backlog(self):
+        logger.info("open backlog")
+
         backlog_element = WebDriverWait(self.driver, 30).until(EC.visibility_of_element_located(
             (By.XPATH, self.backlog_element_xpath_locator)))
         backlog_element.click()
 
     def create_issue(self, issue_value):
+        logger.info("creating issue with value: " + issue_value)
+
         create_issue_element = WebDriverWait(self.driver, 30).until(
             EC.visibility_of_element_located((By.XPATH, self.create_issue_element_xpath_locator)))
         create_issue_element.click()
@@ -45,38 +73,26 @@ class DashboardPage():
         create_issue_textarea_element.send_keys(issue_value)
         create_issue_textarea_element.send_keys(Keys.RETURN)
 
-    def delete_issue_v1(self, issue_timestamp):
-        search_element = WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located((By.XPATH, self.search_element_locator)))
-        search_element.click()
-        search_element.send_keys(issue_timestamp)
-        search_element.click()
+    def delete_issue(self, issue_timestamp):
+        # TODO - show Hod what is contain and what is exact with contains(., 456) and contains(., '456')
 
-        # if time.sleep(2) not added - it will catch different element!
-        time.sleep(2)
-        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH,
-                                                                               f"//*[@id='ak-main-content']//div[@data-test-id='software-backlog.card-list.id-2']//div[contains(., '{issue_timestamp}')]"))).click()
+        logger.info("deleting issue with value: " + issue_timestamp)
+
+        self.search(issue_timestamp).click()
+
+        time.sleep(3)
+        self.driver.find_element(By.XPATH, self.action_button_locator).click()
+        self.driver.find_element(By.XPATH, self.delete_button_locator).click()
+        self.driver.find_elements(By.XPATH, self.delete_buttons_approve_locator)[1].click()
+
+    def delete_issue_v1(self, issue_timestamp):
+        logger.info("deleting issue with value: " + issue_timestamp)
+
+        self.search(issue_timestamp).click()
+
         WebDriverWait(self.driver, 10).until(
             EC.visibility_of_element_located((By.XPATH, self.action_button_locator))).click()
         WebDriverWait(self.driver, 10).until(
             EC.visibility_of_element_located((By.XPATH, self.delete_button_locator))).click()
         WebDriverWait(self.driver, 10).until(
             EC.visibility_of_any_elements_located((By.XPATH, self.delete_buttons_approve_locator)))[1].click()
-
-    def delete_issue(self, issue_timestamp):
-        # TODO - show Hod what is contain and what is exact with contains(., 456) and contains(., '456')
-
-        time.sleep(2)
-        search_element = self.driver.find_element(By.XPATH, self.search_element_locator)
-        search_element.click()
-        search_element.send_keys(issue_timestamp)
-        search_element.click()
-
-        time.sleep(1)
-        self.driver.find_element(By.XPATH,
-                                 f"//*[@id='ak-main-content']//div[@data-test-id='software-backlog.card-list.id-2']//div[contains(., '{issue_timestamp}')]").click()
-
-        time.sleep(3)
-        self.driver.find_element(By.XPATH, self.action_button_locator).click()
-        self.driver.find_element(By.XPATH, self.delete_button_locator).click()
-        self.driver.find_elements(By.XPATH, self.delete_buttons_approve_locator)[1].click()
--- tests/ui/issue_test.py
+++ tests/ui/issue_test.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import pytest
 
 from src.jira_test_framework.ui.dashboard_page import DashboardPage
+from src.logger import logger
 from tests.ui.constants import PROJECT_NAME
 from tests.ui.driver_factory import get_chrome_driver
 from tests.ui.ui_utils import login
@@ -17,7 +18,7 @@ def driver():
 
 def test_create_issue(driver):
     timestamp_str = str(datetime.now().timestamp())
-    issue_text = "from test ui - can delete " + timestamp_str
+    issue_text = "ISSUE from ui test " + timestamp_str
 
     login(driver)
     dashboard_page = DashboardPage(driver)
@@ -26,10 +27,14 @@ def test_create_issue(driver):
     dashboard_page.click_backlog()
 
     dashboard_page.create_issue(issue_text)
-    # TODO    need assertion + log
+    assert dashboard_page.is_issue_exist(issue_text) == True
+    logger.info("issue: " + issue_text + " was created")
+
     # two options:
     # dashboard_page.delete_issue(timestamp_str)
     dashboard_page.delete_issue_v1(timestamp_str)
+    assert dashboard_page.is_issue_exist(issue_text) == False
+    logger.info("issue: " + issue_text + " was deleted")
     time.sleep(2)
 
 # TODO def create_empty_issue
